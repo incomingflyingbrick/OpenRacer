3910db6b43fe76f672cc41fcd9b0fabb47458644
incomingflyingbrick <vurtnesaerdna@gmail.com>
2022-09-30 10:21:59 +0800

    fix inference problem

--- ai_racer/object_detection.py
+++ ai_racer/object_detection.py
@@ -11,6 +11,8 @@ img_que = queue.Queue()
 
 def cameraCallback(change):
     # self.get_logger().info(str(change['new']))
+    tolerance = 40
+    tolerance_hight = 30
     image = change['new']
     try:
         batch = [image]
@@ -18,43 +20,40 @@ def cameraCallback(change):
         result = model(tf.convert_to_tensor(data))
         box_tensor = result['detection_boxes']
         class_tensor = result['detection_classes']
-        print('class:')
-        print(class_tensor)
+        # print('class:')
+        # print(class_tensor)
         detected_index = tf.where(tf.equal(1.0,class_tensor[0]))
         score_index = detected_index[0][0]
         score_tensor = result['detection_scores']
-        print('score shape:')
+        # print('score shape:')
         if score_tensor[0][score_index] >=0.6:
             #print(draw boxes)
             box_data = box_tensor[0]
             i = box_data[score_index]
             centre_x =  int((int(i[3]*image.shape[1])-int(i[1]*image.shape[1]))/2) +int(i[1]*image.shape[1])
             centre_y =  int((int(i[2]*image.shape[0])-int(i[0]*image.shape[0]))/2) +int(i[0]*image.shape[0])
-            cv2.line(image,(centre_x,centre_y),(int(image.shape[1]/2),int(image.shape[0]/2)),(50, 134, 76), 2)
+            cv2.line(image,(centre_x,centre_y),(int(image.shape[1]/2),int(image.shape[0]/2)),(0, 0,200), 2)
 
             cv2.rectangle(image, (int(i[1]*image.shape[1]), int(i[0]*image.shape[0])),
-                        (int(i[3]*image.shape[1]), int(i[2]*image.shape[0])), (255, 134, 76), 3)
+                        (int(i[3]*image.shape[1]), int(i[2]*image.shape[0])), (255, 0, 0), 2)
             target_centre_x = int(image.shape[1]/2)
             target_centre_y = int(image.shape[0]/2)
-            if centre_x - target_centre_x >0:
-                print('turn right')
-            else:
-                print('turn left')
+            if centre_x - target_centre_x > 0 and abs(centre_x - target_centre_x)>tolerance:
+                print('turn right==============>')
+            elif centre_x - target_centre_x < 0 and abs(centre_x - target_centre_x)>tolerance:
+                print('<==============turn left')
 
-            if centre_y - target_centre_y > 0:
-                print('reverse')
-            else:
-                print('forward')
+            if centre_y - target_centre_y > 0 and abs(centre_y - target_centre_y)>tolerance_hight:
+                print('==========reverse==========')
+            elif centre_y - target_centre_y < 0 and abs(centre_y - target_centre_y)>tolerance_hight:
+                print('========================================forward========================================')
             
-            
-
     except Exception as e:
         logging.error(traceback.format_exc())
     print("inference done")
-    #cv2.imshow("Result", image)
-    cv2.rectangle(image, (int(image.shape[1]/2)-20, int(image.shape[0]/2)-20),
-                     (int(image.shape[1]/2)+20, int(image.shape[0]/2)+20), (0, 200, 0), 1)
-    img_que.put(image)
+    cv2.rectangle(image, (int(image.shape[1]/2)-tolerance, int(image.shape[0]/2)-tolerance_hight),
+                     (int(image.shape[1]/2)+tolerance, int(image.shape[0]/2)+tolerance_hight), (0, 200, 0), 2)
+    #img_que.put(image)
 
 
 
@@ -82,20 +81,18 @@ print('model success load')
 
 
 
-camera = CSICamera(width=328, height=246, capture_fps=2)
+camera = CSICamera(width=328, height=246, capture_fps=10)
+#camera = CSICamera(width=656, height=492, capture_fps=20)
 camera.running = True
 camera.observe(cameraCallback, names='value')
-
-while True:
-    try:
-        print('running')
-        fuck = img_que.get()
-        print(fuck.shape)
-        cv2.imshow('test_windows',fuck)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    except Exception as e:
-        logging.error(traceback.format_exc())
+print('camerta init success')
+# while True:
+#     try:
+#         cv2.imshow('camera_view',img_que.get())
+#         if cv2.waitKey(1) & 0xFF == ord('q'):
+#             break
+#     except Exception as e:
+#         logging.error(traceback.format_exc())
 
 # image = '/Users/yazhoujiang/Downloads/dog4.png'
 # image = cv.imread(image)
